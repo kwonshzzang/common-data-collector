7c402a3b142993151bb1716c588dda6bf0b8d492
kwonshzzang <kwonshzzang@gmail.com>
2025-04-01 17:11:02 +0900

    update application.yml

--- src/main/java/kr/co/kwonshzzang/commondatacollector/config/KafkaProducerConfig.java
+++ src/main/java/kr/co/kwonshzzang/commondatacollector/config/KafkaProducerConfig.java
@@ -20,14 +20,19 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.schema-registry-url}")
+    private String schemaRegistryUrl;
+
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
+        System.out.println("Bootstrap Server : --------------" + bootstrapServers);
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
         config.put(ProducerConfig.ACKS_CONFIG, "all");
-        config.put("schema.registry.url", "http://localhost:8081");
+        config.put("schema.registry.url", schemaRegistryUrl);
         return new DefaultKafkaProducerFactory<>(config);
     }
 
